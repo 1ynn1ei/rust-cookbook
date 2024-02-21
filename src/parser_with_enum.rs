9356428c590bfc5e8a58e957f60c2641ee6c8f9e
SophieLeichtle <sophie.leichtle@gmail.com>
2024-02-21 11:43:18 +0100

    added changing value of enum

--- src/parser_with_enum.rs
+++ src/parser_with_enum.rs
@@ -63,8 +63,46 @@ impl ParserTree {
     pub fn add(&mut self, child: TestEnum, parent: PoolRef) -> PoolRef {
         let mut child_node = Node::new(child);
         child_node.set_parent(parent);
-        self.pool.add(child_node)
+        let child_ref = self.pool.add(child_node);
+        let parent_node = self.pool.get_mut(parent);
+        parent_node.children.push(child_ref);
+        child_ref
+    }
+
+    pub fn change_b(&mut self, node_ref: PoolRef, new_value: String) -> Result<(), &'static str> {
+
+        let node_type = &mut self.pool.get_mut(node_ref).node_type;
+        match node_type {
+            TestEnum::A => {
+                return Err("Expected TestEnum B, but found TestEnum A")
+            },
+            TestEnum::B(ref mut string) => {
+                *string = new_value;
+            }
+        }
+
+        Ok(())
     }
 }
 
 
+#[test]
+pub fn test_change_b() {
+    let mut tree = ParserTree::new(TestEnum::B("A".to_string()));
+    
+    match &tree.pool.get(0).node_type {
+        TestEnum::A => assert!(false),
+        TestEnum::B(str) => assert_eq!(*str, "A".to_string())
+    }
+
+    match tree.change_b(0, "B".to_string()) {
+        Ok(()) => {},
+        Err(_str) => assert!(false),
+    }
+
+    match &tree.pool.get(0).node_type {
+        TestEnum::A => assert!(false),
+        TestEnum::B(str) => assert_eq!(*str, "B".to_string())
+    }
+
+}
